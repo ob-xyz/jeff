2e9f4076de08987e5ba948c59d219948d96262bd
wn <csignore@whril.com>
2024-10-02 14:37:58 -0400

    x

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <div className="container">
       <div className="inner-content">
       <a className="none" href="https://app.jeffamzn.com/subscription/form"><img src={logo} /></a>
-      <p>Business in the front. Party in the back.</p>
+      <p>Nobody wants to work anymore.</p>
       <a href="https://app.jeffamzn.com/subscription/form">Subscribe</a>
       </div>
     </div>
