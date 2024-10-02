21b1e5a3261116c583d8cb17fcc8d8665b9f152b
wn <csignore@whril.com>
2024-10-02 15:27:51 -0400

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <div className="container">
       <div className="inner-content">
       <a className="none" href="https://app.jeffamzn.com/subscription/form"><img src={logo} /></a>
-      <p>Jeffamzn is nothing without its readers.</p>
+      <p>Read by some of the most influential people in the world.</p>
       <a href="https://app.jeffamzn.com/subscription/form">Subscribe</a>
       </div>
     </div>
