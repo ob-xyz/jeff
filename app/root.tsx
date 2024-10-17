aaed43df7217cc98fba0ca7713cf139356bd8180
wn <csignore@whril.com>
2024-10-17 13:36:28 -0400

    xx

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Daily Newsletter on Business & Tech at Jeffamzn",
-  description: "Sign up for free, 5-minute newsletter that helps you get locked in."
+  description: "Sign up for free, 5-minute newsletter that helps you make profit."
 });
 
 export default function App() {
