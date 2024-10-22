0017c63571e8d7f5ec172e494a092b4c8ecaa1d6
wn <csignore@whril.com>
2024-10-22 15:55:00 -0400

    da

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
          <p>I'm Jeffamzn. And, every day I'll send you all the business news that matters.</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form" class="listmonk-form">
-          <input type="text" name="name" placeholder="First name" />
+          <input type="text" name="name" placeholder="Firstname" />
           <p><input className="email" type="email" name="email" required placeholder="Email" /></p>
           <p><input className="submit" type="submit" value="Subscribe" /></p>
           <input id="9cc7a" type="hidden" name="l" checked value="9cc7aab4-4f23-4f01-8297-8138cfa4670d" />
