bd048ca648bbac36c9883420c42fdd607239cc19
wn <csignore@whril.com>
2024-11-12 01:06:40 -0500

    jhbj

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
          <p>Gotta minute? Join my free newsletter. And you'll quickly see why it's one of the smartest ways to start your day.</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
-          <input type="text" name="name" placeholder="First Name" />
+          <input type="text" name="name" placeholder="Name" />
           <p><input className="email" type="email" name="email" required placeholder="Email" /></p>
           <p><input className="submit" type="submit" value="Subscribe" /></p>
           <input id="b2958" type="hidden" name="l" checked value="b2958bdd-ba18-4541-aff7-58c773cbcb7d" />
