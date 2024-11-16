c2b3883505f40c0bf9cd8f7496ea902a932e010c
wn <csignore@whril.com>
2024-11-16 09:06:29 -0500

    cv

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Jeffamzn",
-  description: "Every morning Jeffamzn delivers a quick news feed about the business world.",
+  description: "Every morning Jeffamzn delivers you a time line about the business world.",
   viewport: "width=device-width,initial-scale=1",
 });
 
