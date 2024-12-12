e7f93a14aef3bea7e4184ae55fe4d497e960ebc2
wn <csignore@whril.com>
2024-12-11 22:04:17 -0500

    sd

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -18,13 +18,11 @@ export default function Index() {
 
 return (
     <div className="container">
-
-            <div className="logo"><img src={logo} /></div>
-
+      <div className="logo"><img src={logo} /></div>
       <div className="inner-content">
       <div className="text">
-         <p>Hi, I'm Jeffamzn!</p>
-         <p>Your no-BS friend making good mornings in the business world even better.</p>
+         <p>Hey there!</p>
+         <p>I'm Jeffamzn, your no-BS friend making good mornings in the business world even better.</p>
          <p>Gotta minute? Join my free newsletter. And you'll quickly see why it's one of the most insightful ways to start your day.</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
