4b9adff76ffa4888d77d17d10d4ac700a80472dc
wn <csignore@whril.com>
2025-01-13 11:46:21 -0500

    xx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,7 +24,7 @@ return (
       <div className="inner-content">
       <div className="text">
          <p>All access. Zero BS news.</p>
-         <p>Real stories and insights from real people in business and tech.<br></br>One daily newsletter.</p>
+         <p>Every day Jeffamzn lights up your email inbox with real stories and insights from real people in business and tech.</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
           {/* <input type="text" name="name" placeholder="Enter Your Name" /> */}
