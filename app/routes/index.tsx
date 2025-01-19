f0544cd4422b477bb138e4b978a1a2e3e61e602b
wn <csignore@whril.com>
2025-01-19 13:11:36 -0500

    11

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -23,8 +23,8 @@ return (
       </div>
       <div className="inner-content">
       <div className="text">
-         <p>Like TechCrunch, but funny.</p>
-         <p>Original stories and insights from thinkers and doers in tech</p>
+         <p>All access. Zero BS news.</p>
+         <p>Original stories and insights from thinkers and doers in business and tech</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
           {/* <input type="text" name="name" placeholder="Enter Your Name" /> */}
