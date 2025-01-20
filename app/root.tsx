76523054b298b255c49dceb8d1501fd989d02df3
wn <csignore@whril.com>
2025-01-19 23:43:42 -0500

    xx

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Daily Newsletter on Business & Tech at Jeffamzn",
-  description: "Daily newsletter with real stories and insights from thinkers and doers in business and tech.",
+  description: "Real stories and insights from citizens in fintech, crypto, AI, longevity, politics, memes, and whatever else is the current thing.",
   viewport: "width=device-width,initial-scale=1",
 });
 
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -23,8 +23,8 @@ return (
       </div>
       <div className="inner-content">
       <div className="text">
-         <p>All access. Zero BS news.</p>
-         <p>Real stories and insights from thinkers and doers in business and tech</p>
+         <p>Like TechCrunch, but funny.</p>
+         <p>Real stories and insights from citizens in fintech, crypto, AI, longevity, politics, memes, and whatever else is the current thing</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
           {/* <input type="text" name="name" placeholder="Enter Your Name" /> */}
