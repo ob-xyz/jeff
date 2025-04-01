ab13dd0a10e2a2dc8520a71856defe5627a77562
wn <csignore@whril.com>
2025-04-01 14:09:29 -0400

    z

--- app/root.tsx
+++ app/root.tsx
@@ -27,8 +27,8 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Jeffamzn - Thank God it's Morning.",
-  description: "Jeffamzn is the front page of wallstreetbets, delivered to your email inbox.",
+  title: "Jeffamzn - Thank God it's Here.",
+  description: "Jeffamzn is the front page of business and tech, delivered to your email inbox every day.",
   viewport: "width=device-width,initial-scale=1",
 });
 
