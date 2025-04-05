3afd0c8405feed36b87d64cb1f7d798e5dfa4bd9
wn <csignore@whril.com>
2025-04-05 18:26:24 -0400

    11

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Jeffamzn",
-  description: "World's #1 Wealth Poasts! Read every weekend by the most wealthy investors and entrepreneurs in the world.",
+  description: "World's #1 Paid Poasts! Read every weekend by the highest paid investors and entrepreneurs in the world.",
   viewport: "width=device-width,initial-scale=1",
 });
 
