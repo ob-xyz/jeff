e6543c73fb72be261a566dc095c52f6a4192c81a
wn <csignore@whril.com>
2025-04-15 08:35:20 -0400

    12

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -32,7 +32,7 @@ return (
           <p><input className="email" type="email" name="email" required placeholder="Email" /></p>
           <p><input className="submit" type="submit" value="Subscribe" /></p>
           <div className="h-captcha" data-sitekey="db0e76a6-3d84-4378-abe6-88526faac9d2"></div>
-          <input id="b2958" type="hidden" name="l" checked value="b2958bdd-ba18-4541-aff7-58c773cbcb7d" />
+          <input id="f854f" type="hidden" name="l" checked value="f854f96d-e421-4c68-8abf-4e6d451b9b9a" />
           <input type="hidden" name="nonce" />
       </form>
       </div>
