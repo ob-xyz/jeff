1bbe6b1791c661c0d84f0f9aec5a58158c939e11
wn <csignore@whril.com>
2025-05-29 20:27:22 -0400

    187

--- .netlify/functions-internal/server.js
+++ .netlify/functions-internal/server.js
@@ -55,7 +55,7 @@ __export(root_exports, {
 var import_react2 = require("@remix-run/react");
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-66U6UCNW.css";
+var global_default = "/build/_assets/global-4SZA3OBE.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = require("react/jsx-dev-runtime"), links = () => [
@@ -161,11 +161,11 @@ function Index() {
           columnNumber: 9
         }, this),
         /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("p", { children: [
-          "Jeffamzn is not the only way to find out what's happening. But it's a pretty good way to find out what's happening. Join our free newsletter to ",
-          /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("b", { children: "find out what's happening in the world." }, void 0, !1, {
+          "Jeffamzn is not the only way to know what's going on in the world. But it's a pretty good way to know what's going on in the world. Join our free newsletter to ",
+          /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("b", { children: "know what's going on in the world." }, void 0, !1, {
             fileName: "app/routes/index.tsx",
             lineNumber: 27,
-            columnNumber: 157
+            columnNumber: 173
           }, this)
         ] }, void 0, !0, {
           fileName: "app/routes/index.tsx",
@@ -247,7 +247,7 @@ function Index() {
 }
 
 // server-assets-manifest:@remix-run/dev/assets-manifest
-var assets_manifest_default = { version: "b586a1e5", entry: { module: "/build/entry.client-3Z3BMBC7.js", imports: ["/build/_shared/chunk-TY4X4K57.js", "/build/_shared/chunk-EETRBLDB.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-JSWKE2DH.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-IEJ627CA.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, cssBundleHref: void 0, url: "/build/manifest-B586A1E5.js" };
+var assets_manifest_default = { version: "40647253", entry: { module: "/build/entry.client-3Z3BMBC7.js", imports: ["/build/_shared/chunk-TY4X4K57.js", "/build/_shared/chunk-EETRBLDB.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-ZBHGPPWR.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-ICR2A2JM.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, cssBundleHref: void 0, url: "/build/manifest-40647253.js" };
 
 // server-entry-module:@remix-run/dev/server-build
 var assetsBuildDirectory = "public/build", future = { unstable_cssModules: !1, unstable_cssSideEffectImports: !1, unstable_vanillaExtract: !1, v2_errorBoundary: !1, v2_meta: !1, v2_routeConvention: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
