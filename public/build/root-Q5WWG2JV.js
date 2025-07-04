fc34331cb63b0b1c0cfe007651b401dd64a8e200
wn <45404661+ob-xyz@users.noreply.github.com>
2025-07-04 00:29:08 -0400

    187

--- public/build/root-Q5WWG2JV.js
+++ public/build/root-Q5WWG2JV.js
@@ -10,7 +10,7 @@ import {
 } from "/build/_shared/chunk-FAW7IH37.js";
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-RSOUPAOY.css";
+var global_default = "/build/_assets/global-PCFOU5EW.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
@@ -88,4 +88,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-U37VEWDR.js.map
+//# sourceMappingURL=/build/root-Q5WWG2JV.js.map
