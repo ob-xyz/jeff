1df4465c4a89846e3e3b1d3a5b9a131c7f5c613d
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-02 00:04:47 -0400

    2

--- public/build/root-CQLVAYZA.js
+++ public/build/root-CQLVAYZA.js
@@ -225,7 +225,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-EQJVPZAR.css";
+var global_default = "/build/_assets/global-UXE2MS5H.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -308,4 +308,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-TZAFH7AZ.js.map
+//# sourceMappingURL=/build/root-CQLVAYZA.js.map
