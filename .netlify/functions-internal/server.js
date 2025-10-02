a0da825d0beb6e1dcf5887ef778d70bfa89be167
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-02 00:07:31 -0400

    11

--- .netlify/functions-internal/server.js
+++ .netlify/functions-internal/server.js
@@ -272,7 +272,7 @@ function Footer() {
 var import_react3 = require("@remix-run/react");
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-E6QVP6FF.css";
+var global_default = "/build/_assets/global-UJJI22E4.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime3 = require("react/jsx-dev-runtime"), links = () => [
--- public/build/root-V6YWMI7B.js
+++ public/build/root-V6YWMI7B.js
@@ -225,7 +225,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-E6QVP6FF.css";
+var global_default = "/build/_assets/global-UJJI22E4.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -308,4 +308,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-EXYF6QTH.js.map
+//# sourceMappingURL=/build/root-V6YWMI7B.js.map
