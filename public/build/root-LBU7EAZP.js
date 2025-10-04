2a156916248a63d211fd7b1e6808363fab748979
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-04 00:37:57 -0400

    1122

--- public/build/root-LBU7EAZP.js
+++ public/build/root-LBU7EAZP.js
@@ -225,7 +225,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-643PMIYY.css";
+var global_default = "/build/_assets/global-GKLNKUQD.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -308,4 +308,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-B723ULAW.js.map
+//# sourceMappingURL=/build/root-LBU7EAZP.js.map
