152cf6493bdd9008825fe06ca72f9fc22307794e
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-04 20:39:04 -0400

    11

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -89,7 +89,7 @@ export default function Index() {
       </div>
       <div className="inner-content4">
             <h3>Delivered straight to your inbox daily.</h3>
-            <p>Join 10,000+ tech execs, founders, and entrepreneurs staying informed about the business world.</p>
+            <p>Join 10,000+ tech execs, founders, and entrepreneurs who like staying informed about the business world.</p>
       </div>
       <div className="inner-content5">
         <div className="box">
