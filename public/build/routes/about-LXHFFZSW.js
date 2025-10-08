f930b340a19c4f7b8874a550904ff19b08fb26c5
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-08 07:29:27 -0400

    1

---
+++ public/build/routes/about-LXHFFZSW.js
@@ -0,0 +1,55 @@
+import {
+  ja1_default
+} from "/build/_shared/chunk-3UAG7PBE.js";
+import {
+  __toESM,
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-4QJKEWZT.js";
+
+// app/routes/about.tsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "content-aboutus", children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "logo", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { src: ja1_default, alt: "The Poast Logo" }, void 0, false, {
+      fileName: "app/routes/about.tsx",
+      lineNumber: 6,
+      columnNumber: 9
+    }, this) }, void 0, false, {
+      fileName: "app/routes/about.tsx",
+      lineNumber: 5,
+      columnNumber: 7
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "content-aboutus2", children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h2", { children: [
+        "We're your fave ",
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("span", { children: "business" }, void 0, false, {
+          fileName: "app/routes/about.tsx",
+          lineNumber: 9,
+          columnNumber: 27
+        }, this),
+        " newsletter."
+      ] }, void 0, true, {
+        fileName: "app/routes/about.tsx",
+        lineNumber: 9,
+        columnNumber: 7
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("p", { children: "Every day of the week Jeffamzn delivers business and tech news that feels good to read\u2014with extra eyes on startups, strange business models, smart insights, and newly minted hot takes. We're your fave newsletter." }, void 0, false, {
+        fileName: "app/routes/about.tsx",
+        lineNumber: 10,
+        columnNumber: 7
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/routes/about.tsx",
+      lineNumber: 8,
+      columnNumber: 7
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/routes/about.tsx",
+    lineNumber: 4,
+    columnNumber: 5
+  }, this);
+}
+export {
+  Index as default
+};
+//# sourceMappingURL=/build/routes/about-LXHFFZSW.js.map
