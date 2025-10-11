2655740ad153074782b8dff41414b6f1098ab17e
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-11 15:28:13 -0400

    11

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -7,7 +7,7 @@ export default function Index() {
       </div>
       <div className="content-aboutus2">
       <h2>We're <span>the internet's</span> fave email.</h2>
-      <p>Got a minute? We'll help you catch the business news that actually matters—from real voices and trusted sources.</p>
+      <p>Got a minute? We'll help you catch the business news that actually matters today.</p>
       </div>
   </div>
   );
