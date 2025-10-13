13c180a19b49ba77f72ab405c79df86886316af6
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-12 23:56:04 -0400

    12

--- public/build/root-K7QK5LHM.js
+++ public/build/root-K7QK5LHM.js
@@ -225,7 +225,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-YBHW6PYO.css";
+var global_default = "/build/_assets/global-KDSVHJKC.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -308,4 +308,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-CTOXMSDL.js.map
+//# sourceMappingURL=/build/root-K7QK5LHM.js.map
