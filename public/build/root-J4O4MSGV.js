573a03979b4fc0efbb853509b2b46033230ae9c3
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-13 00:18:14 -0400

    12

--- public/build/root-J4O4MSGV.js
+++ public/build/root-J4O4MSGV.js
@@ -225,7 +225,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-4BVUBP6L.css";
+var global_default = "/build/_assets/global-5XHWFDLH.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -308,4 +308,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-OPRH6IYZ.js.map
+//# sourceMappingURL=/build/root-J4O4MSGV.js.map
