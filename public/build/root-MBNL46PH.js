4099ba84ac7a8025cb6ac2ce5b171b2f65bad11a
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-13 01:14:33 -0400

    88

--- public/build/root-MBNL46PH.js
+++ public/build/root-MBNL46PH.js
@@ -225,7 +225,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-2AWSK6NT.css";
+var global_default = "/build/_assets/global-A62AE3JV.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -308,4 +308,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-DZYV7FQR.js.map
+//# sourceMappingURL=/build/root-MBNL46PH.js.map
