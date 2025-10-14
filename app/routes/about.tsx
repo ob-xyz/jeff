fb3dc42c4dbc1554ae779ba0099d0dd97e8d5472
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-14 15:18:00 -0400

    111

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -6,7 +6,7 @@ export default function Index() {
         <img src={logo} alt="The Poast Logo" />
       </div>
       <div className="content-aboutus2">
-      <h2>We're <span>your fave</span> newsletter.</h2>
+      <h2>We are <span>Silicon Valley's</span> newsletter.</h2>
       <p>Every day of the week, Jeffamzn covers the latest news on business, the economy, and everything that actually matters to real people doing business in the business world.</p>
       </div>
   </div>
