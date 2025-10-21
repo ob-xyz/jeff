a2b5f2b8bab79fc1ecbcad9eba88def29012dc36
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-21 15:33:42 -0400

    1122

--- public/build/root-N6DAJ36K.js
+++ public/build/root-N6DAJ36K.js
@@ -225,7 +225,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-A7NJHRCM.css";
+var global_default = "/build/_assets/global-FRKDX6NL.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -308,4 +308,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-JRXITACX.js.map
+//# sourceMappingURL=/build/root-N6DAJ36K.js.map
