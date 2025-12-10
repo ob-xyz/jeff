19ba12a5ac70d917b886c861557f89531d7014be
wn <45404661+ob-xyz@users.noreply.github.com>
2025-12-09 22:55:49 -0500

    112

--- public/build/root-4ZZH4UXE.js
+++ public/build/root-4ZZH4UXE.js
@@ -227,7 +227,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-CIC7OMPX.css";
+var global_default = "/build/_assets/global-BJUBIA2Q.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -310,4 +310,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-6BLBPGDR.js.map
+//# sourceMappingURL=/build/root-4ZZH4UXE.js.map
