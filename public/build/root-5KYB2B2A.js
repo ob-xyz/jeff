099045f9276ecbbf13d414432aa7e9b30e78d9ed
wn <45404661+ob-xyz@users.noreply.github.com>
2025-12-29 23:28:47 -0500

    11

--- public/build/root-5KYB2B2A.js
+++ public/build/root-5KYB2B2A.js
@@ -232,7 +232,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-XFUYDPI7.css";
+var global_default = "/build/_assets/global-R4HMQ6SM.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -315,4 +315,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-H65QMUBN.js.map
+//# sourceMappingURL=/build/root-5KYB2B2A.js.map
