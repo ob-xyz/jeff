7ca63fa08ac9507fc11a3040486022788d613d24
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-09 01:10:05 -0500

    1

--- public/build/root-JHD5OEKK.js
+++ public/build/root-JHD5OEKK.js
@@ -232,7 +232,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-MZY6GHSQ.css";
+var global_default = "/build/_assets/global-3PMROEHS.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -315,4 +315,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-A54QHGSO.js.map
+//# sourceMappingURL=/build/root-JHD5OEKK.js.map
