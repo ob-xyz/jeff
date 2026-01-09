c54ed4293a4f42c1c47e420dfd4fd21efc414407
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-08 23:52:59 -0500

    12

--- public/build/root-6Z5LBQBX.js
+++ public/build/root-6Z5LBQBX.js
@@ -232,7 +232,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-L37LKWZD.css";
+var global_default = "/build/_assets/global-H27YN3XS.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -315,4 +315,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-CMYAIRAD.js.map
+//# sourceMappingURL=/build/root-6Z5LBQBX.js.map
