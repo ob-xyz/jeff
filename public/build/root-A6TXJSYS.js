f550009db8986aaeadedcb4cee845bf6216f763e
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-09 01:11:26 -0500

    89

--- public/build/root-A6TXJSYS.js
+++ public/build/root-A6TXJSYS.js
@@ -232,7 +232,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-3PMROEHS.css";
+var global_default = "/build/_assets/global-VSGLDWZD.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -315,4 +315,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-JHD5OEKK.js.map
+//# sourceMappingURL=/build/root-A6TXJSYS.js.map
