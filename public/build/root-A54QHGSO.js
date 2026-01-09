53f8d0fa608063a9c18229fcd50203d541625858
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-09 01:06:03 -0500

    228

--- public/build/root-A54QHGSO.js
+++ public/build/root-A54QHGSO.js
@@ -232,7 +232,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-AQHILTPP.css";
+var global_default = "/build/_assets/global-MZY6GHSQ.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -315,4 +315,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-AVJTPB42.js.map
+//# sourceMappingURL=/build/root-A54QHGSO.js.map
