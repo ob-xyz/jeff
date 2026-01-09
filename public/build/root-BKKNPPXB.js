a59c8cc0312b017247a5b5b3b71f53fc43f31b31
wn <45404661+ob-xyz@users.noreply.github.com>
2026-01-09 01:21:32 -0500

    2

--- public/build/root-BKKNPPXB.js
+++ public/build/root-BKKNPPXB.js
@@ -232,7 +232,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-AUHCBH3B.css";
+var global_default = "/build/_assets/global-X2AY4OV6.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -315,4 +315,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-PFH225C3.js.map
+//# sourceMappingURL=/build/root-BKKNPPXB.js.map
