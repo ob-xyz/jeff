b887a1a1ef560fb0c1da4f8bd458a7e5230d14a8
wn <45404661+ob-xyz@users.noreply.github.com>
2026-02-10 23:52:34 -0500

    1122

--- app/routes/subscribe.tsx
+++ app/routes/subscribe.tsx
@@ -33,8 +33,8 @@ export default function Index() {
         </div>
       </div>
       <div className="inner-content2">
-      <h2>Start winning with <span>Jeffamazon</span></h2>
-        <Link className="text" to="/subscribe">Don't waste your time scrolling to stay ahead. Jeffamazon is the <span>1 minute antidote that keeps 42,000+ people in the loop</span> about what matters across the world.</Link>
+      <h2>Win more with <span>Jeffamazon</span></h2>
+        <Link className="text" to="/subscribe">Don't waste time scrolling to stay ahead. <span>Jeffamazon is the 1 minute antidote that keeps 42,000+ people in the loop</span> about what matters across the world.</Link>
       </div>
     </div>
   );
