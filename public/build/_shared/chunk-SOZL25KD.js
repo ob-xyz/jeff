f96e18a44064d047a25b8fb21f83f340d0e3b78f
wn <45404661+ob-xyz@users.noreply.github.com>
2026-03-04 11:01:54 -0500

    11

---
+++ public/build/_shared/chunk-SOZL25KD.js
@@ -0,0 +1,11 @@
+// public/img/ads/carouselads.gif
+var carouselads_default = "/build/_assets/carouselads-IOGQ5ITP.gif";
+
+// public/img/ads/videoads.gif
+var videoads_default = "/build/_assets/videoads-5GUDWDD4.gif";
+
+export {
+  carouselads_default,
+  videoads_default
+};
+//# sourceMappingURL=/build/_shared/chunk-SOZL25KD.js.map
