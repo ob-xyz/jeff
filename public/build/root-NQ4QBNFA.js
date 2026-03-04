20a7c4491e72ab26ca435850de8bff9112142d3c
wn <45404661+ob-xyz@users.noreply.github.com>
2026-03-04 11:07:23 -0500

    1

--- public/build/root-NQ4QBNFA.js
+++ public/build/root-NQ4QBNFA.js
@@ -194,7 +194,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-IURNNT3V.css";
+var global_default = "/build/_assets/global-D3FLNEFQ.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -277,4 +277,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-Q4LBRAAT.js.map
+//# sourceMappingURL=/build/root-NQ4QBNFA.js.map
