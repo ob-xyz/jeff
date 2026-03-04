d9d612684e4e66d22c29c9dae444caab04d00e58
wn <45404661+ob-xyz@users.noreply.github.com>
2026-03-04 11:21:15 -0500

    11

--- public/build/root-IJLKOQQE.js
+++ public/build/root-IJLKOQQE.js
@@ -194,7 +194,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-MFRBW7UG.css";
+var global_default = "/build/_assets/global-RB2TFU33.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -277,4 +277,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-54U3WZUG.js.map
+//# sourceMappingURL=/build/root-IJLKOQQE.js.map
