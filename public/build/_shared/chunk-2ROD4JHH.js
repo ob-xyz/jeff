261e70852e0c66187c5b88aaf504843fa64594ec
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-18 23:48:57 -0400

    1

---
+++ public/build/_shared/chunk-2ROD4JHH.js
@@ -0,0 +1,7 @@
+// public/img/ads/imageads.png
+var imageads_default = "/build/_assets/imageads-HHJOWAPY.png";
+
+export {
+  imageads_default
+};
+//# sourceMappingURL=/build/_shared/chunk-2ROD4JHH.js.map
